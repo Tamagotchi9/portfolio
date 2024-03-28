d4ebb366a8ee87c8530ccab1ea0a8a002f3a61f9
Tamagotchi9 <voloshko999@gmail.com>
2024-03-28 15:12:07 +0200

    fix assets

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,47 +1,54 @@
 <template>
-  <header class="lg:w-1/2 static lg:sticky lg:top-0 lg:max-h-screen flex flex-col justify-between lg:py-24">
+  <header
+    class="lg:w-1/2 static lg:sticky lg:top-0 lg:max-h-screen flex flex-col justify-between lg:py-24"
+  >
     <div>
-        <div class="flex flex-col items-center lg:flex-row lg:items-start">
-            <div class="rounded-full w-24 h-24 border-slate-200 border-2 mr-4">
-                <img class="rounded-full w-full h-auto"  src="../assets/thumbnail.jpeg" alt="me">
-            </div>
-            <div>
-                <h1 class="text-4xl font-bold text-slate-200 tracking-tight sm:text-5xl">
-                    Dmytro Voloshko
-                </h1>
-                <h2 class="mt-3 text-xl font-medium tracking-tight text-slate-200 sm:text-xl">
-                    Frontend Developer at Checkbox
-                </h2>
-                <p class="mt-4 max-w-xs leading-normal">
-                    Frontend enthusiast, constantly growing, delivering great UI experience.
-                </p>
-            </div>
+      <div class="flex flex-col items-center lg:flex-row lg:items-start">
+        <div class="rounded-full w-24 h-24 border-slate-200 border-2 mr-4">
+          <img class="rounded-full w-full h-auto" :src="HeroThumbnail" alt="me" />
         </div>
-      <navigation :active="active"/>
+        <div>
+          <h1 class="text-4xl font-bold text-slate-200 tracking-tight sm:text-5xl">
+            Dmytro Voloshko
+          </h1>
+          <h2 class="mt-3 text-xl font-medium tracking-tight text-slate-200 sm:text-xl">
+            Frontend Developer at Checkbox
+          </h2>
+          <p class="mt-4 max-w-xs leading-normal">
+            Frontend enthusiast, constantly growing, delivering great UI experience.
+          </p>
+        </div>
+      </div>
+      <navigation :active="active" />
     </div>
-    <social/>
+    <social />
   </header>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import Navigation from '@/components/Navigation.vue';
+import Navigation from '@/components/Navigation.vue'
 import Social from '@/components/Social.vue'
+import Thumbnail from '@/assets/thumbnail.jpeg'
 
 export default defineComponent({
   name: 'AppIntroduction',
-    props: {
-      active: {
-        type: String,
-        default: ''
-      }
-    },
+  props: {
+    active: {
+      type: String,
+      default: ''
+    }
+  },
   components: {
     Navigation,
     Social
+  },
+  computed: {
+    HeroThumbnail() {
+      return Thumbnail
+    }
   }
 })
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,49 +1,51 @@
 <template>
-    <section id="projects" ref="projects" class="scroll-mt-16 lg:scroll-mt-24">
-        <h3 class="block lg:hidden font-semibold text-2xl text-slate-200 mb-3">Projects</h3>
-        <div>
-            <ul class="group/list">
-                <li v-for="(project, idx) in projects" :key="idx" class="mb-12">
-                    <projects-item :item="project" />
-                </li>
-            </ul>
-        </div>
-    </section>
+  <section id="projects" ref="projects" class="scroll-mt-16 lg:scroll-mt-24">
+    <h3 class="block lg:hidden font-semibold text-2xl text-slate-200 mb-3">Projects</h3>
+    <div>
+      <ul class="group/list">
+        <li v-for="(project, idx) in projects" :key="idx" class="mb-12">
+          <projects-item :item="project" />
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
-import ProjectsItem from '@/components/ProjectsItem.vue';
+import ProjectsItem from '@/components/ProjectsItem.vue'
+import meetupsThumbnail from '@/assets/images/meetups.png'
+import cryptoThumbnail from '@/assets/images/cryptonomicon.png'
+import kanbanThumbnail from '@/assets/images/kanban-board.png'
 
 export default {
-    name: 'AppProjects',
-    components: {ProjectsItem},
-    computed: {
-        projects () {
-            return [
-                {
-                    name: 'Meetups service',
-                    description: 'Web app for planning offline meetups. View meetups, create meetups, join meetups, detailed information and meetup agenda. Authentication, calendar view of meetups and more.',
-                    image: '/src/assets/images/meetups.png',
-                    link: 'https://meetup-service.netlify.app/'
-                },
-                {
-                    name: 'Crypto dashboard',
-                    description: 'Crypto dashboard - check cryptocurrencies prices in real time.',
-                    image: '/src/assets/images/cryptonomicon.png',
-                    link: 'https://leafy-valkyrie-e1ccfa.netlify.app/'
-                },
-                {
-                    name: 'Kanban board',
-                    description: 'Kanban board - simple scrum board to plan sprints with your team!',
-                    image: '/src/assets/images/kanban-board.png',
-                    link: 'https://kanban-board123.netlify.app/'
-                }
-            ]
+  name: 'AppProjects',
+  components: { ProjectsItem },
+  computed: {
+    projects() {
+      return [
+        {
+          name: 'Meetups service',
+          description:
+            'Web app for planning offline meetups. View meetups, create meetups, join meetups, detailed information and meetup agenda. Authentication, calendar view of meetups and more.',
+          image: meetupsThumbnail,
+          link: 'https://meetup-service.netlify.app/'
+        },
+        {
+          name: 'Crypto dashboard',
+          description: 'Crypto dashboard - check cryptocurrencies prices in real time.',
+          image: cryptoThumbnail,
+          link: 'https://leafy-valkyrie-e1ccfa.netlify.app/'
+        },
+        {
+          name: 'Kanban board',
+          description: 'Kanban board - simple scrum board to plan sprints with your team!',
+          image: kanbanThumbnail,
+          link: 'https://kanban-board123.netlify.app/'
         }
+      ]
     }
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
